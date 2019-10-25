a3a195a7357449e9c840e740ea008ebda4b6117a
Sam Frees1de <snovvcrash@protonmail.ch>
2019-10-25 19:31:52 +0300

    Завершение работы над Bitterman

--- pwn-kingdom/3-bitterman/bitterman/bitterman-ghidra.c
+++ pwn-kingdom/3-bitterman/bitterman/bitterman-ghidra.c
@@ -1,25 +1,28 @@
-// bitterman.c
+// Compile: gcc -g -fno-stack-protector -no-pie -o bitterman-ghidra bitterman-ghidra.c
 
-int read_nbytes(char *dst,size_t nbytes) {
+#include <stdio.h>
+#include <unistd.h>
+
+int read_nbytes(char *dst, size_t nbytes) {
   int iVar1;
   ssize_t sVar2;
   long lVar3;
   int i;
-  
+
   i = 0;
-  while( true ) {
-    if (nbytes <= (ulong)(long)i) {
+  while (1) {
+    if (nbytes <= (long)(long)i)
       return i;
-    }
-    sVar2 = read(0,dst + i,1);
+
+    sVar2 = read(0, dst + i, 1);
     if (sVar2 == 0) break;
     iVar1 = i + 1;
     lVar3 = (long)i;
     i = iVar1;
-    if (dst[lVar3] == '\n') {
+    if (dst[lVar3] == '\n')
       return iVar1;
-    }
   }
+
   return i;
 }
 
@@ -29,20 +32,23 @@ int main(int argc,char **argv) {
   char buf [64];
   char username [64];
   size_t size;
-  
+
   puts("> What\'s your name? ");
   fflush(stdout);
-  read_nbytes(username,0x40);
-  printf("Hi, %s\n",username);
+  read_nbytes(username, 0x40);
+  printf("Hi, %s\n", username);
+
   puts("> Please input the length of your message: ");
   fflush(stdout);
-  __isoc99_scanf(&DAT_004008c4,&nbytes);
+  scanf("%llu",&nbytes);
+
   puts("> Please enter your text: ");
   fflush(stdout);
-  iVar1 = read_nbytes(buf,nbytes);
+  iVar1 = read_nbytes(buf, nbytes);  // <-- УЯЗВИМЫЙ КОД
   if (iVar1 != 0) {
     puts("> Thanks!");
     fflush(stdout);
   }
+
   return 0;
 }
